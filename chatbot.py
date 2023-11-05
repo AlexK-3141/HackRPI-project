69ed87d08abffe9cef347e64cc181fd8a54da160
AlexK-3141 <alexkuang0314@gmail.com>
2023-11-04 22:22:39 -0400

    comment i guess??

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+#chatbot that acts like a therapist and helps user sort out emotions (no ui(no tkinter))
 from transformers import pipeline
 
 transcriber = pipeline(task="automatic-speech-recognition")
